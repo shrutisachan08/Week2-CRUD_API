00fd7c4b008e81d99a653b73aada4a77a0db1509
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-07-14 18:10:26 +0530

    Stage 5 completed

--- main.py
+++ main.py
@@ -21,23 +21,30 @@ tasks=[
 class Task(BaseModel):
     title:str
     done:bool
-@app.get("/")
+@app.get("/",summary="Home",
+    description="Returns API information.")
 def home():
     return {
         "name": "FastAPI",
         "version":"1.0",
         "endpoints": ["/tasks"]
     }
-@app.get("/health")
+@app.get("/health",
+    summary="Health Check",
+    description="Checks if the API is running.")
 def health():
     return{
         "status":"ok"
     }
-@app.get("/tasks")
+@app.get("/tasks",
+    summary="Get all tasks",
+    description="Returns a list of all tasks.")
 def get_tasks():
     return tasks
 
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}",
+    summary="Get task by ID",
+    description="Returns a single task by its ID.")
 def get_task(task_id:int):
     for task in tasks:
         if task["id"]==task_id:
@@ -46,7 +53,8 @@ def get_task(task_id:int):
         status_code=404,
         detail="Task not found"
     )
-@app.post("/tasks",status_code=status.HTTP_201_CREATED)
+@app.post("/tasks",status_code=status.HTTP_201_CREATED,   summary="Create a task",
+    description="Creates a new task.")
 def create_task(task:Task):
     new_task={
         "id":len(tasks)+1,
@@ -56,7 +64,8 @@ def create_task(task:Task):
     tasks.append(new_task)
     return new_task
 
-@app.put("/tasks/{task_id}")
+@app.put("/tasks/{task_id}" ,summary="Update task",
+    description="Updates an existing task.")
 def update_task(task_id:int,updated_task:Task):
     for task in tasks:
         if task["id"]==task_id:
@@ -67,14 +76,13 @@ def update_task(task_id:int,updated_task:Task):
         status_code=404,
         detail="Task not found"
     )
-@app.delete("/tasks/{task_id}")
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT,summary="Delete task",
+    description="Deletes a task.")
 def delete_task(task_id:int):
     for task in tasks:
         if task["id"]==task_id:
             tasks.remove(task)
-            return{
-                "message":"task deleted successfully"
-            }
+            return
     raise HTTPException(
         status_code=404,
         detail="Task not found"
