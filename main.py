ffc0635ba6dfc016e8407b9fc479957566e9f0e2
Shruti Sachan <sachan.shruti2004@gmail.com>
2026-07-14 13:02:53 +0530

    Stage 2 completed

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 app=FastAPI()
+tasks=[
+    {
+        "id":1,
+        "title":"study FastAPI",
+        "done":False
+    },
+    {
+        "id":2,
+        "title":"Complete Assignment",
+        "done":False
+    },
+    {
+        "id":3,
+        "title":"Go for a walk",
+        "done":True
+    }
+]
 @app.get("/")
 def home():
     return {
@@ -12,3 +29,16 @@ def health():
     return{
         "status":"ok"
     }
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id:int):
+    for task in tasks:
+        if task["id"]==task_id:
+            return task
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
